f5918ebf1092ecf515997d5a8f0c7d1b222c6ffa
darkbrgo <610075647@qq.com>
2016-09-25 19:39:43 +0800

    Add files via upload

---
+++ exercise_03.py
@@ -0,0 +1,10 @@
+for a in range(60):
+  import os
+  i = os.system('cls')
+  print" "*a,"hh    hh zzzzzzz  xx    xx"
+  print" "*a,"hh    hh    zzz    xx  xx "
+  print" "*a,"hhhhhhhh   zzz       xx   "
+  print" "*a,"hh    hh  zzz      xx  xx "
+  print" "*a,"hh    hh zzzzzzz  xx    xx"
+  import time
+  time.sleep(0.2)
