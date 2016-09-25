28ad8b77212a595373beca113f9fb93873e31b8b
darkbrgo <610075647@qq.com>
2016-09-26 00:30:57 +0800

    Add files via upload

---
+++ kk.py
@@ -0,0 +1,50 @@
+
+line0 ="　　　　　　 ★★　　　"
+line1 ="　         ★　　★"
+line2 ="        ★★　　  ★★"
+line3 ="      ★★　　　　  ★★"
+line4 ="    ★★　　　　　　  ★★"
+line5 ="  ★★　　　　　　　　  ★★"
+line6 =" ★★　　　　　　　　　　 ★★"
+line7 ="★★　　　　●　　　　●　　　★★"
+line8 ="★★　　　　　　　○　　　　　★★"
+line9 ="★★　　　◎　　　　　　◎　　★★"
+line10="★★　　　　　　　●　　　　　★★"
+line11="★★　　　　　　　　　　　　★★"
+line12="  ★★　　　　　　　　　 ★★"
+line13="     ★★★★★★★★★★"
+     
+ymax=20                     
+for y in range(20):       
+    import os                             
+    i = os.system('cls')   
+    b=(y)
+    for j in range(b):
+        print " "  
+    print line0  
+    print line1  
+    print line2  
+    print line3  
+    print line4  
+    print line5  
+    print line6  
+    print line7  
+    print line8  
+    print line9
+    print line10
+    print line11
+    print line12
+    print line13
+    import time
+    time.sleep(0.05)
+    import os                             
+    i = os.system('cls')  
+for i in range(30):
+    print" "
+print"      ★★★★★★★★★★★★★★★★★★★★  "
+print"   ★★★★★★★★★★★★★★★★★★★★★★★ "
+print" ★★★★★★★★★★★★★★★★★★★★★★★★★"
+print"★★★★★★★★★★★★★★★★★★★★★★★★★★★"
+print" ★★★★★★★★★★★★★★★★★★★★★★★★★★★"
+print"   ★★★★★★★★★★★★★★★★★★★★★★★★★"
+    
